4ac38cbf867eac0b0b83ff6b4df616512e20018b
MASANAMATHONSI <masanamathonsi050209@gmail.com>
2026-04-09 17:34:06 +0200

    The adding of the form file and the components atht are used to run the form

---
+++ Notes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Note_App__CRUD_
+{
+    public partial class Notes : Form
+    {
+        public Notes()
+        {
+            InitializeComponent();
+        }
+
+        private void Notes_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

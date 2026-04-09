77b5aef592edc0afb473a5891ee353c7cabd8a0f
MASANAMATHONSI <masanamathonsi050209@gmail.com>
2026-04-09 17:40:21 +0200

    Ive added another form that will act as the navigation of the note application

---
+++ NoteNav.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Note_App__CRUD_
+{
+    public partial class NoteNav : Form
+    {
+        public NoteNav()
+        {
+            InitializeComponent();
+        }
+    }
+}

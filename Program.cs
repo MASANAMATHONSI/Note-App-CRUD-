c0d080c0593a6bc8c90f3f8959597fd01bbc0389
MASANAMATHONSI <masanamathonsi050209@gmail.com>
2026-04-09 17:30:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Note_App__CRUD_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
